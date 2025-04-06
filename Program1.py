39d6658007caad02c8ef2c00461bb3e7ab8c258a
mads-98 <98mc1719@gmail.com>
2025-04-05 21:36:11 -0400

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,40 @@
+# Madison Cronk
+# 0582982
+# Program #1
+# COMS-170-01: Winter 2025
+# Due: 1/24/2025
+# Program #1 - Simple Output Program
+# ------------------------------------------------------------------
+# Variable              Type        Purpose
+# ------------------------------------------------------------------
+# strName               string      stores name information
+# strDegree             string      stores desgree program information
+# strGoal               string      stores information regarding career goal
+# strGrad               string      stores target graduation date 
+# strCartoon            string      stores name of my favorite cartoon character
+
+# Create variables and assign values
+strName = 'Madison Cronk'
+strDegree = 'Data Analytics A.A.S.'
+strGoal = 'healthcare data analyst'
+strGrad = 'May 2026'
+strCartoon = 'Hello Kitty'
+
+# Perform calculations and operations
+
+
+# Display output to user
+print('Hello! My name is', strName,'.')
+print('I am currently studying for the following degree:', strDegree,'.')
+print('Upon completion of the program I plan to pursue a job as a', strGoal,'.')
+print('If everything goes as planned I will graduate in', strGrad,'.')
+print('My favorite cartoon character is', strCartoon,'.')
+
+# Add Output of final program as Comments
+##=================== RESTART: /Users/mads/Desktop/Program1.py ===================
+##Hello! My name is Madison Cronk .
+##I am currently studying for the following degree: Data Analytics A.A.S. .
+##Upon completion of the program I plan to pursue a job as a healthcare data analyst .
+##If everything goes as planned I will graduate in May 2026 .
+##My favorite cartoon character is Hello Kitty .
+
