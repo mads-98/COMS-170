2b56b23ea3c6c0efaf052379be43a35c2598df3a
MADISON CRONK <98mc1719@gmail.com>
2025-04-06 21:49:39 -0400

    debugging previous assignment

--- Program4.py
+++ Program4.py
@@ -13,36 +13,26 @@
 # intResponse           integer     user response to the generated addition equation
 # intAnswer             integer     stores the value of the correct answer to the given equation
 
+import random
+
 # Create variables and assign values
 print("Welcome to Mr.Euclid's addition quiz!")
 strContinue = "y"
 
 # Perform calculations and operations
 while strContinue == "y":
-    import random
     intNum1 = random.randint(20, 29)
     intNum2 = random.randint(20, 29)
     intAnswer = intNum1 + intNum2
     intResponse = int(input(f"What is the result of {intNum1} + {intNum2}? "))
-    while intResponse != intAnswer:
-        intResponse = int(input("That is incorrect. Please try again. "))
-    print("Correct! Great job.")
+    if intResponse == intAnswer:
+        print("Correct! Great job.")
+    else:
+        print(f"That is incorrect. The correct answer is {intAnswer}.")
     strContinue = input("Would you like to continue the quiz? Please enter 'y' or 'n'. ")
-        
-
 # Display output to user
-print("Thank you for your participation! Come back to practice anytime. ")
+while strContinue != 'y':
+    print("Thank you for your participation! Come back to practice anytime. ")
+while strContinue is not ['y','n']:
+    print("ERROR - Please enter a valid menu option")
 
-# Add Output of final program as Comments
-# ------------------------------------------------------------------
-##= RESTART: /Volumes/USB DISK/MCC/ASSIGNMENTS BY SEMESTER/WINTER 2025/COMS-170/program4.py
-##Welcome to Mr.Euclid's addition quiz!
-##What is the result of 24 + 23? 25
-##That is incorrect. Please try again. 25
-##That is incorrect. Please try again. 47
-##Correct! Great job.
-##Would you like to continue the quiz? Please enter 'y' or 'n'. y
-##What is the result of 27 + 26? 53
-##Correct! Great job.
-##Would you like to continue the quiz? Please enter 'y' or 'n'. n
-##Thank you for your participation! Come back to practice anytime. 
